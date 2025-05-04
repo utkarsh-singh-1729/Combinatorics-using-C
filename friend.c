34a690079354e7c27a5cea4292370100a8c69a90
Utkarsh Singh <99618831+utkarsh-singh-1729@users.noreply.github.com>
2025-05-04 07:59:06 +0530

    Add files via upload

---
+++ friend.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void){
+    //name
+    int name = scanf("Enter Your Name :");
+
+    //age
+    int age = get_int("Enter Your Age :");
+
+    //HomeTown
+    string city = get_string("Enter Your HomeTown :");
+
+    //phone
+    string phone = get_string("Enter Your Phone Number :");
+}
