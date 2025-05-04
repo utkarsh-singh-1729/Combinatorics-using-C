de1c11d89cc50ec4b61b493031f2bab1470b0696
Utkarsh Singh <99618831+utkarsh-singh-1729@users.noreply.github.com>
2025-05-04 07:59:36 +0530

    Add files via upload

---
+++ mann.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <cs50.h>
+int x = get_int();
+switch(x){
+    case 1:
+        printf("One!\n");
+        break;
+
+    case 2:
+        printf("Two!\n");
+        break;
+
+    case 3:
+        printf("Three!\n");
+        break;
+
+    default:
+        printf("Sorry!\n")
+
+
+}
+
